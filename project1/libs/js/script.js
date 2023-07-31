925a6e5e3e29b46bc6b816c794c77fec96724371
Darfy <darfy009@gmail.com>
2023-07-31 21:12:21 +0100

    -- General code optimization

--- project1/libs/js/script.js
+++ project1/libs/js/script.js
@@ -10,6 +10,28 @@ $(window).on("load", function () {
     layers: [tile],
   }).fitWorld();
 
+  const countryInfo = L.easyButton("fa-solid fa-circle-info", function (btn, map) {
+    $("#info").modal("show");
+  }).addTo(map);
+
+  const countryWiki = L.easyButton("fa-solid fa-globe", function (btn, map) {
+    $("#wiki").modal("show");
+  }).addTo(map);
+
+  const countryWeather = L.easyButton(
+    "fa-solid fa-question",
+    function (btn, map) {
+      $("#weather").modal("show");
+    }
+  ).addTo(map);
+
+  const countryCurrency = L.easyButton(
+    "fa-solid fa-question",
+    function (btn, map) {
+      $("#currency").modal("show");
+    }
+  ).addTo(map);
+
   loadSelectCountries();
 
   // load user location
@@ -20,15 +42,15 @@ $(window).on("load", function () {
   }
 
   // remove existing layers
-  function clearLayers () {
-    map.eachLayer(function(layer){
+  function clearLayers() {
+    map.eachLayer(function (layer) {
       if (layer instanceof L.Polygon) {
         map.removeLayer(layer);
       }
-  });
+    });
   }
 
-  //highlight country borders
+  // on changing selected countries
   let countrySelect = document.getElementById("countrySelect");
   countrySelect.addEventListener("change", () => {
     // remove existing layers
@@ -36,18 +58,55 @@ $(window).on("load", function () {
     const index = countrySelect.selectedIndex;
     const selectCountry = countrySelect.options[index];
     let selectCountryId = selectCountry.id;
+    // highlight country borders
     $.ajax({
       url: "./libs/php/getCountryBorders.php",
-      type: "POST",
+      type: "GET",
       dataType: "json",
       success: ({ status, data }) => {
         if (status.name === "ok") {
+          // console.log(data)
           const coords = data[selectCountryId];
           // reverse array of latitude and longitutde values to display borders correctly
           const latlngs = latlngsReverse(coords);
           const polygon = L.polygon(latlngs, { color: "purple" }).addTo(map);
           map.fitBounds(polygon.getBounds());
         }
+      },
+      error: (jqXHR) => {
+        console.log(jqXHR.responseText);
+      },
+    });
+
+    // country info modal
+    $.ajax({
+      url: "./libs/php/getCountryInfo.php",
+      type: "GET",
+      dataType: "json",
+      data: { selectCountryId },
+      success: ({ status, data }) => {
+        if (status.name === "ok") {
+          // console.log(data)
+          $("#country").html(data["geonames"][0]["countryName"]);
+          $("#capital").html(data["geonames"][0]["capital"]);
+          $("#population").html(data["geonames"][0]["population"]);
+          $("#area").html(data["geonames"][0]["areaInSqKm"]);
+          $("#continent").html(data["geonames"][0]["continentName"]);
+        }
+        // wikipedia modal
+        let countryName = data["geonames"][0]["countryName"];
+        $.ajax({
+          url: "./libs/php/getCountryWiki.php",
+          type: "GET",
+          dataType: "json",
+          data: { countryName },
+          success: ({ status, data }) => {
+            if (status.name === "ok") {
+              console.log(data);
+              
+            }
+          }
+        })
       },
       error: (jqXHR) => {
         console.log(jqXHR.responseText);
@@ -93,7 +152,6 @@ function loadUserLocation({ coords: { latitude, longitude } }) {
       if (status.name === "ok") {
         const countryCode = data.countryCode;
         // console.log(countryCode);
-
         let countrySelect = document.getElementById("countrySelect");
         countrySelect.id = countryCode;
         for (let i = 0; i < countrySelect.options.length; i++) {
@@ -117,4 +175,3 @@ function latlngsReverse(array) {
   }
   return array;
 }
-
---
+++ project1/libs/php/getCountryInfo.php
@@ -0,0 +1,38 @@
+<?php
+
+	require_once __DIR__ . "../../../../vendor/autoload.php";
+
+	ini_set('display_errors', 'On');
+	error_reporting(E_ALL);
+
+	$dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . "../../../" );
+	$dotenv->safeLoad();
+
+	$username = $_ENV["API_USERNAME"];
+
+	$executionStartTime = microtime(true);
+
+	$url='http://api.geonames.org/countryInfoJSON?formatted=true&country='. $_REQUEST['selectCountryId'] . '&username=' . $username .'&style=full';
+
+	$ch = curl_init();
+	curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+	curl_setopt($ch, CURLOPT_URL,$url);
+
+	$result=curl_exec($ch);
+
+	curl_close($ch);
+
+	$decode = json_decode($result, true);	
+
+	$output['status']['code'] = "200";
+	$output['status']['name'] = "ok";
+	$output['status']['description'] = "success";
+	$output['status']['returnedIn'] = intval((microtime(true) - $executionStartTime) * 1000) . " ms";
+	$output['data'] = $decode;
+	
+	header('Content-Type: application/json; charset=UTF-8');
+
+	echo json_encode($output); 
+
+?>
